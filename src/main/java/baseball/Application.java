c5200917c616b7c8d3df38a461b8d4dc1902cd12
Changjin <quritis@naver.com>
2022-10-01 00:02:49 +0900

    깃 처음해봐서 테스트중

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms.*;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        //System.out.println(pickNumberInRange(1, 999));
+
     }
 }
